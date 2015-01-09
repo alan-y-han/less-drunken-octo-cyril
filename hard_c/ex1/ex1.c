46fff66649edd37bd2e34bd03f156dd0d9ae397b
Alan Han <alan.y.han@gmail.com>
2015-01-09 23:03:19 +0000

    ex 3 4 5

--- hard_c/ex1/ex1.c
+++ hard_c/ex1/ex1.c
@@ -1,7 +1,8 @@
-// #include <stdio.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[]) {
-	puts("Hello world.");
+	
+    puts("Hello world.");
 	
 	return 0;
 }
---
+++ hard_c/ex1/ex3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	int age = 10;
+    int height = 72;
+    
+    printf("I am %d years old. \n", age);
+    printf("I am %d cm tall.\n", height);
+    // printf("asd\ffadsf\n");
+	
+	return 0;
+}
